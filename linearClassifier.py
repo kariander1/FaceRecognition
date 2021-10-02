435b7c7d706d3a7fb865f560476b08c41472f00b
Shai Yehezkel <shaiyehezkel@campus.technion.ac.il>
2021-10-02 12:47:33 +0300

    Added Dropout and changed a bit the transformations

--- linearClassifier.py
+++ linearClassifier.py
@@ -11,67 +11,6 @@ import torchvision
 import torchvision.transforms as transforms
 from prettytable import PrettyTable
 
-
-class CNN(nn.Module):
-    """CNN."""
-
-    def __init__(self):
-        """CNN Builder."""
-        super(CNN, self).__init__()
-
-        self.conv_layer = nn.Sequential(
-
-            # Conv Layer block 1
-            nn.Conv2d(in_channels=3, out_channels=32, kernel_size=3, padding=1),
-            nn.BatchNorm2d(32),
-            nn.ReLU(inplace=True),
-            nn.Conv2d(in_channels=32, out_channels=64, kernel_size=3, padding=1),
-            nn.ReLU(inplace=True),
-            nn.MaxPool2d(kernel_size=2, stride=2),
-
-            # Conv Layer block 2
-            nn.Conv2d(in_channels=64, out_channels=128, kernel_size=3, padding=1),
-            nn.BatchNorm2d(128),
-            nn.ReLU(inplace=True),
-            nn.Conv2d(in_channels=128, out_channels=128, kernel_size=3, padding=1),
-            nn.ReLU(inplace=True),
-            nn.MaxPool2d(kernel_size=2, stride=2),
-            nn.Dropout2d(p=0.05),
-
-            # Conv Layer block 3
-            nn.Conv2d(in_channels=128, out_channels=256, kernel_size=3, padding=1),
-            nn.BatchNorm2d(256),
-            nn.ReLU(inplace=True),
-            nn.Conv2d(in_channels=256, out_channels=256, kernel_size=3, padding=1),
-            nn.ReLU(inplace=True),
-            nn.MaxPool2d(kernel_size=2, stride=2),
-        )
-
-        self.fc_layer = nn.Sequential(
-            nn.Dropout(p=0.1),
-            nn.Linear(4096, 1024),
-            nn.ReLU(inplace=True),
-            nn.Linear(1024, 512),
-            nn.ReLU(inplace=True),
-            nn.Dropout(p=0.1),
-            nn.Linear(512, 10)
-        )
-
-    def forward(self, x):
-        """Perform forward."""
-
-        # conv layers
-        x = self.conv_layer(x)
-
-        # flatten
-        x = x.view(x.size(0), -1)
-
-        # fc layer
-        x = self.fc_layer(x)
-
-        return x
-
-
 # Define NET class with RELU activation
 class Net(nn.Module):
     def __init__(self, dropout_rate, filter_size, pooling_size, num_of_filters, num_of_filters_2, num_of_filters_3,
@@ -310,7 +249,7 @@ for i in range(1, 2):
     fields = {}
     csv_name = r'accuracy_chart.csv'
     batch_size = 8
-    filter_size = 5
+    filter_size = 3
     epochs = 30
     dropout_rate = 0.05
     num_of_filters = 32
@@ -347,7 +286,7 @@ for i in range(1, 2):
     train_transform = transforms.Compose(
         [transforms.ToTensor(),
          transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5)),
-         transforms.RandomHorizontalFlip(0.1),
+         # transforms.RandomHorizontalFlip(0.1),
          # transforms.ColorJitter(brightness=.5, hue=.3),
          transforms.RandomCrop(32, padding=4),
          transforms.RandomHorizontalFlip(),
